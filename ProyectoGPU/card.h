4b802cf5f6c47d215d6f6099fc96c3666f53d6d4
Hojin <hojink1996@gmail.com>
2021-06-22 00:32:12 -0400

    Add initial structure for the holdem project

---
+++ ProyectoGPU/card.h
@@ -0,0 +1,27 @@
+#pragma once
+
+enum class Suit
+{
+	Clubs,
+	Diamonds,
+	Hearts,
+	Spades
+};
+
+enum class Value
+{
+	Ace,
+	Two,
+	Three,
+	Four,
+	Five,
+	Six,
+	Seven,
+	Eight,
+	Nine,
+	Ten,
+	Jack,
+	Queen,
+	King
+};
+
---
+++ ProyectoGPU/deck.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Deck
+{
+private:
+	int cards[52];
+
+};
